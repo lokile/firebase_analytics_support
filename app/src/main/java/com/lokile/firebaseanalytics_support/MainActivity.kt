0c3dc322209e80f99358af17ccafc6ac0a5a80e9
lokile <107489661+lokile@users.noreply.github.com>
2022-07-08 18:03:25 +0700

    initial

---
+++ app/src/main/java/com/lokile/firebaseanalytics_support/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.lokile.firebaseanalytics_support
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
