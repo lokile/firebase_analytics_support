f9869e0746de9a838b21a64b27a0397fc06e0824
lokile <107489661+lokile@users.noreply.github.com>
2022-07-08 18:31:26 +0700

    initial

--- library/src/main/java/com/lokile/firebase_analytics_support/utils.kt
+++ library/src/main/java/com/lokile/firebase_analytics_support/utils.kt
@@ -1,9 +1,31 @@
 package com.lokile.firebase_analytics_support
 
+import android.app.Application
+import com.google.firebase.analytics.ktx.analytics
 import com.google.firebase.crashlytics.FirebaseCrashlytics
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.remoteconfig.ktx.remoteConfig
+import com.lokile.applibraries.managers.*
 
 
 fun handleException(throwable: Throwable) {
     FirebaseCrashlytics.getInstance().recordException(throwable)
     throwable.printStackTrace()
+}
+
+fun initFirebase(app: Application, configList: List<RemoteConfigValue>, allowLogEvent: Boolean) {
+    EventTrackingManager.init(app, Firebase.analytics, allowLogEvent)
+    setCurrentAppVersion(
+        app.packageManager.getPackageInfo(app.packageName, 0).versionName
+    )
+    RemoteConfigManager.reloadConfig(
+        Firebase.remoteConfig,
+        configList.plus(
+            DefaultRemoteConfigValues.USER_SEGMENT_NAME
+        )
+    ) { fromPrevious, isUpdated, isSuccess ->
+        if (isSuccess) {
+            setUserSegmentName(DefaultRemoteConfigValues.USER_SEGMENT_NAME.value as String)
+        }
+    }
 }
